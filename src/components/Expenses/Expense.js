a7542653ae872b58ae0c5e3969afbbf35699a324
gurmukh singh <gurmukhs300@gmail.com>
2023-03-16 09:36:44 +1100

    using mag to render data

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -13,30 +13,16 @@ const Expense = (props) => {
 
     return (
         <div>
-            <ExpenseFilter seleted={seletedDate} onChangeFilter={selectHandler} />
         <Card className='expenses'>
-            <ExpenseItem
-            title={props.expenses[0].title}
-            amount={props.expenses[0].amount}
-            date={props.expenses[0].date}>
-            </ExpenseItem>
-            <ExpenseItem
-            title={props.expenses[1].title}
-            amount={props.expenses[1].amount}
-            date={props.expenses[1].date}>
-            </ExpenseItem>
+            <ExpenseFilter seleted={seletedDate} onChangeFilter={selectHandler} />
 
-            <ExpenseItem
-            title={props.expenses[2].title}
-            amount={props.expenses[2].amount}
-            date={props.expenses[2].date}>
-            </ExpenseItem>
+            {props.expenses.map(expense => <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}>
+            </ExpenseItem>)}
 
-            <ExpenseItem
-            title={props.expenses[3].title}
-            amount={props.expenses[3].amount}
-            date={props.expenses[3].date}>
-            </ExpenseItem>
+            
         </Card>
         </div>
     )
